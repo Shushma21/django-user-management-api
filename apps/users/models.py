2482dfade91003a2e4ba66a72d25cfc4846e3c3e
shushma <shushma@gmail.com>
2026-03-16 13:25:34 +0530

    Initial project setup with Django and PostgreSQL

---
+++ apps/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.db import models
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, default="user")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
