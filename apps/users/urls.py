5c2f6a32b820839f4a6e381afce84a301012e844
shushma <shushma@gmail.com>
2026-04-13 13:05:22 +0530

    Added UserViewSet and user CRUD endpoints using DRF router

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .import views
-from .views import RegisterUserAPIView,admin_only
+from django.urls import path,include
+#from .import views
+from rest_framework.routers import DefaultRouter
+from .views import RegisterUserAPIView,UserViewSet,admin_only
+
+router = DefaultRouter()
+router.register(r'',UserViewSet)
 
 urlpatterns = [
 		path('register/',RegisterUserAPIView.as_view()),
 		path('admin_only/',admin_only),
+		path('',include(router.urls)),
 	]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,20 +1,27 @@
 from  rest_framework.generics import CreateAPIView
-from .models import UserProfile, User
+from .models import User
 from .serializers import  UserSerializer
 from .permissions import IsAdminUserRole
 from  rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 class RegisterUserAPIView(CreateAPIView):
-	#queryset = UserProfile.objects.all()
 	queryset = User.objects.all()
-	#serializer_class = UserProfileSerializer
 	serializer_class = UserSerializer
-
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated,IsAdminUserRole])
 def admin_only(request):
-	return Response({"message":"Only admin can access this"})
+        return Response({"message":"Only admin can access this"})
+
+
+class UserViewSet(ModelViewSet):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+	
+	def get_permissions(self):
+		if self.action in['list','destroy']:
+			return[IsAuthenticated(),IsAdminUserRole()]
+		return [IsAuthenticated()]
